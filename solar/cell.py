6e9a1a5e8ce02d9829762f2b66cebb02815e0b59
ufshk <ufsheikh@live.ca>
2019-06-21 09:40:27 -0400

    update efficiency numbers

--- solar/cell.py
+++ solar/cell.py
@@ -4,10 +4,10 @@ class SolarCell:
     def __init__(self, area, temperature):
         self.area = area
         # efficiency of solar energy conversion to electrical energy
-        self.efficiency = 23.7 
+        self.efficiency = .237 
         # temp_coeff is the percentage decrease
         # in the cells' efficiency for every degree above 25C
-        self.temp_coefficient = -0.29
+        self.temp_coefficient = -.0029
         # the internal temperature of the cells/array
         self.temperature = temperature
         
@@ -16,4 +16,6 @@ class SolarCell:
             efficiency = self.efficiency - self.temp_coefficient * (self.temperature - 25)
             return efficiency
         return self.efficiency
-
+    
+    def energy_converted(self)
+        return SolarDay.energy_received() * total_efficiency_decrease()
