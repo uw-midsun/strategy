5e10144cd419be594e9d8459c282f183f73f71b8
Clarke VandenHoven <clarkevandenhoven@gmail.com>
2019-06-22 18:01:09 -0400

    STRAT-38 Convert rolldown test data into usable data (#7)

--- dynamics/rolldownconverter.py
+++ dynamics/rolldownconverter.py
@@ -1,44 +1,42 @@
 import numpy as np
 import matplotlib.pyplot as plt
-# TODO: add data loading for actual data
+import pandas as pd
+from parserdcsv import clean
+from scipy import interpolate
 # Test data
-m = 640 # kg
-Crr = 0.0015
-g = 9.81
-rho = 1.225 
-CdA = 0.15
-vinit = 20
-t = np.linspace(0,800,10000)
-def power(lst, power):
-    # take a list and raise every element to a power
-    return [ x ** power for x in lst]
-# Assuming the data is evenly spaced
-dt = t[1] - t[0]
-v = []
-v.append(vinit)
-i = 0
-while v[i] >= 0.5:
-    v.append(v[i] - ((Crr * m * g + CdA * rho * v[i] ** 2 / 2) * dt / m))
-    i += 1
-t = t[0:len(v)]
-# The numpy function gradient gives a second order approximation of the derivative of the velocity
-a = np.gradient(v,dt)
-# We now want to find the relationship between force and velocity
-# Given that F = ma = Crr * m * g + 1/2 * rho * CdA * v^2
-# We can fit a quadratic curve to our F vs v plot to find CdA and Crr
+def generate_test_data():
+    global m, Crr, g, rho, CdA, vinit, t, dt, v
+    m = 640 # kg
+    Crr = 0.0015
+    g = 9.81
+    rho = 1.225 
+    CdA = 0.15
+    vinit = 25
+    t = np.linspace(0,800,10000)
+    dt = t[1] - t[0]
+    v = []
+    v.append(vinit)
+    i = 0
+    while v[i] >= 0.5:
+        v.append(v[i] - ((Crr * m * g + CdA * rho * v[i] ** 2 / 2) * dt / m))
+        i += 1
+    t = t[0:len(v)]
+generate_test_data()
+data = clean("~/midsun/dynamics/rolldowndata/RollDown2019-06-02-8.csv")
+x = data['time']
+y = data['average_velocity']
+fuzz = 5
+f = interpolate.interp1d(x[0::fuzz], y[0::fuzz], kind = 'cubic')
+t = np.linspace(x.iloc[1], x.iloc[-fuzz], 10000)
+v = f(t)
+polyv = np.polyfit(t,v,3)
+vsmooth = polyv[-4] * t ** 3 + polyv[-3] * t ** 2 + polyv[-2] * t + polyv[-1]
+polya = np.polyder(polyv)
+a = polya[-3] * t ** 2 + polya[-2] * t + polya[-1]
+poly = np.polyfit(vsmooth, a, 2)
+plt.plot(t,v)
+plt.show()
 
-A = np.array([[sum(power(v,4)), sum(power(v,3)), sum(power(v,2))],
-              [sum(power(v,3)), sum(power(v,2)), sum(v)],
-              [sum(power(v,2)), sum(v), len(v)]])
-
-b = [0,0,0]
-for point in range(len(v)):
-    b[0] += v[point] ** 2 * m * a[point]
-    b[1] += v[point] * m * a[point]
-    b[2] += a[point] * m
-
-B = np.array(b)
-x = np.linalg.solve(A,B)
-CdAcalc = -x[0] * 2 / rho
-Crrcalc = -x[2] / (m * g)
-print(CdAcalc, Crrcalc)
+CdAcalc = -poly[0] * 2 * m / rho
+Crrcalc = -poly[2] /  g
+print("Your calculated CdA is {0:.5f} and your calculated Crr is {1:.5f}".format(CdAcalc, Crrcalc))
