ec2e70056074f93d9a2fa5eb23c7dacc13b88940
Melody Lui <melodylui@Melodys-MacBook-Pro.local>
2019-07-27 13:41:32 -0400

    added nice haves

--- dynamics/rolldownconverter.py
+++ dynamics/rolldownconverter.py
@@ -3,40 +3,70 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from parserdcsv import clean
 from scipy import interpolate
-# Test data
-def generate_test_data():
-    global m, Crr, g, rho, CdA, vinit, t, dt, v
-    m = 640 # kg
-    Crr = 0.0015
-    g = 9.81
-    rho = 1.225 
-    CdA = 0.15
-    vinit = 25
-    t = np.linspace(0,800,10000)
-    dt = t[1] - t[0]
-    v = []
-    v.append(vinit)
-    i = 0
-    while v[i] >= 0.5:
-        v.append(v[i] - ((Crr * m * g + CdA * rho * v[i] ** 2 / 2) * dt / m))
-        i += 1
-    t = t[0:len(v)]
-generate_test_data()
-data = clean("~/midsun/dynamics/rolldowndata/RollDown2019-06-02-8.csv")
+import sys 
+import os.path
+my_path = os.path.abspath(os.path.dirname(__file__))
+path = os.path.join(my_path, 'rolldowndata/test.csv') 
+data = clean(path)
 x = data['time']
 y = data['average_velocity']
-fuzz = 5
-f = interpolate.interp1d(x[0::fuzz], y[0::fuzz], kind = 'cubic')
-t = np.linspace(x.iloc[1], x.iloc[-fuzz], 10000)
-v = f(t)
-polyv = np.polyfit(t,v,3)
-vsmooth = polyv[-4] * t ** 3 + polyv[-3] * t ** 2 + polyv[-2] * t + polyv[-1]
-polya = np.polyder(polyv)
-a = polya[-3] * t ** 2 + polya[-2] * t + polya[-1]
-poly = np.polyfit(vsmooth, a, 2)
-plt.plot(t,v)
-plt.show()
+CdAmin = 0
+CdAmax = 1
+Crrmin = 0.0015
+Crrmax = 0.1
+precision = 300 
+def generate_test_datas(CdAmin = CdAmin, CdAmax = CdAmax, Crrmin = Crrmin, Crrmax = Crrmax, vinit = y.iloc[0], time = x.tolist(), precision=precision):
+    m = 660 # kg
+    g = 9.81
+    rho = 1.225
+    combos = []
+    for i in range(precision):
+        CdA = CdAmin + (CdAmax - CdAmin) * i / precision
+        combo = []
+        for j in range(precision):
+            Crr = Crrmin + (Crrmax - Crrmin) * j / precision 
+            v = []
+            v.append(vinit)
+            for t in range(1, len(x)):
+                dt = time[t] - time[t - 1]
+                next_v_half = v[t - 1] - (((Crr * m * g) + (CdA * rho * v[t - 1] ** 2 / 2)) * (dt / 2) / m) 
+                next_v = v[t - 1] - (((Crr * m * g) + (CdA * rho * next_v_half ** 2 / 2)) * dt / m)
+                if next_v < 0:
+                    next_v = 0
+                v.append(next_v)
+            combo.append(v)
+        combos.append(combo)
+    return combos
 
-CdAcalc = -poly[0] * 2 * m / rho
-Crrcalc = -poly[2] /  g
-print("Your calculated CdA is {0:.5f} and your calculated Crr is {1:.5f}".format(CdAcalc, Crrcalc))
+def diff(l1, l2):
+    diff = 0
+    for i in range(len(l1)):
+        diff += (l1[i] - l2[i]) ** 2
+    return diff
+if __name__ == '__main__':
+    velocities = y.tolist()
+    plt.plot(x,y, '--bo')
+    #plt.show()
+    
+    min_SSE = 100000000000000
+    datas = generate_test_datas()
+    for i in range(len(datas)):
+        for j in range(len(datas[i])):
+            SSE = diff(velocities, datas[i][j])
+            if SSE < min_SSE:
+                min_SSE = SSE
+                a,b = i,j
+    print(min_SSE)
+    print(a,b)
+    Crrmeas = Crrmin + (Crrmax - Crrmin) * b / precision
+    CdAmeas = CdAmin + (CdAmax - CdAmin) * a / precision
+    plt.plot(x, datas[a][b], '--co')
+    plt.show()
+    v = [y.iloc[0]]
+    for t in range(1,len(x)):
+        dt = x.tolist()[t] - x.tolist()[t-1]
+        next_v_half = v[t - 1] - ((Crrmeas * 660 * 9.81) + (CdAmeas * 1.225 * (v[t - 1] ** 2))) * ((dt/2) / 660)
+        next_v = v[t - 1] - ((Crrmeas * 660 * 9.81) + (CdAmeas * 1.225 * (next_v_half ** 2))) * (dt / 660)
+        v.append(next_v)
+    print(v)
+    print(Crrmeas, CdAmeas)
