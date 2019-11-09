939c7322561dc533e0563e572f2d3a5a55c84040
ufshk <ufsheikh@live.ca>
2019-11-09 18:39:33 -0500

    Added class attributes for tracking measurements over time

--- auxloss/auxsystems.py
+++ auxloss/auxsystems.py
@@ -1,19 +1,36 @@
 class AuxSystem:
-    def __init__(self, current, voltage, time):
+    def __init__(self, initial_current, initial_voltage):
         """
         Initialize Auxiliary System object
         """
-        self.current = current # amount of current in amperes flowing through auxiliary system
-        self.voltage = voltage # potential electrical difference of auxiliary system
-        self.time = time # time in seconds
+        self.time = 0
+        self.time_old = 0
+        self.current_old = initial_current
+        self.voltage_old = initial_voltage
+        self.current = initial_current # amount of current in amperes flowing through auxiliary system
+        self.voltage = initial_voltage # potential electrical difference of auxiliary system
     
-    def energy_consumed(self):
+    def energy_consumed(self, time_new, new_current, new_voltage):
         """
         Calculate energy consumed by Auxiliary system.
         Power (W) = Current (A) x Voltage (V)
         Energy (J) = Power (W) x Time (s)
         Energy = Current (A) x Voltage (V) x Time (s)
         """
-        energy_consumed = self.current * self.voltage * self.time
+        # two timepoints to determine energy loss of system over period
+        self.time_old = self.time
+        self.time = time_new 
+
+        # current will be divided into two for an average between the two timepoints
+        # could be several points of time period in an array?
+        self.current_old = self.current
+        self.current = new_current
+
+        # voltage will be divided into two for an average between the two timepoints
+        # could be several points of time  period in an array?
+        self.voltage_old = self.voltage
+        self.voltage = new_voltage
+        
+        energy_consumed = ((self.current + self.current_old)/2) * ((self.voltage + self.voltage_old)/2) * (self.time - self.time_old)
         return energy_consumed
 
