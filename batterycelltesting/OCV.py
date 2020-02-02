57805cdfcdf718da9a4185db4e9f4b6169a873e6
Aamer <akhandaamer@gmail.com>
2020-02-02 13:57:13 -0500

    made coding changes as requested

--- batterycelltesting/OCV.py
+++ batterycelltesting/OCV.py
@@ -1,21 +1,18 @@
 import numpy as np
 import pandas as pd
-import random
 import matplotlib.pyplot as plt
 import matplotlib.ticker as mtick
 import os
+def weighting (current, valid_current):
+   low = min(valid_current)
+   high = max(valid_current)
+   if current >= 0:
+      weight = 1 - current / high #Weights added based on closeness to 0A
+   elif current <0:
+      weight = 1 - current / low
+   return weight
 
-
-def Weighting (Current,ValidCurrent):
-   low = min(ValidCurrent)
-   high = max(ValidCurrent)
-   if Current >= 0:
-      weight = 1 - Current/high #Weights added based on closeness to 0A
-   elif Current <0:
-      weight = 1 - Current/low
-   return weight;
-
-def Graphing (filename):
+def graphing (filename):
    ax = plt.axes()
    plt.title(filename + " OCV Visualization")
 
@@ -30,50 +27,47 @@ def Graphing (filename):
    ax.yaxis.set_major_formatter(mtick.FormatStrFormatter('%.6f'))
 
    plt.scatter(current,voltage)
-   plt.savefig('OCVDataGraph.png', bbox_inches='tight') ##Creating a new file each
+   plt.savefig((filename) + '.png', bbox_inches='tight') ##Creating a new file each
    plt.clf()
    
-def Calculation():
+def calculation():
    stdV = np.std(voltage)
    avgV = np.mean(voltage)
    print("Average Voltages for all samples: %s" % avgV)
-   ValidVoltage = []
-   ValidCurrent = []
+   valid_voltage = []
+   valid_current = []
    Weights = []
    i = 0
    for v in voltage:
-       if v < avgV+3*stdV and v > avgV-3*stdV:
-           ValidVoltage.append(voltage[i]) ##Filtering outlier Voltages out
-           ValidCurrent.append(current[i])
+       if v < avgV + 3 * stdV and v > avgV - 3 * stdV:
+           valid_voltage.append(voltage[i]) ##Filtering outlier Voltages out
+           valid_current.append(current[i])
        i = i + 1
 
 
-   for x in ValidCurrent:
-       Weights.append(Weighting(x,ValidCurrent)) #Adds a weighting based on current
+   for x in valid_current:
+       Weights.append(weighting(x,valid_current)) #Adds a weighting based on current
        
    weightedsumV = 0
-   for i in range(0, len(ValidVoltage)):
-      weightedsumV = weightedsumV + ValidVoltage[i]*Weights[i]
+   for i in range(0, len(valid_voltage)):
+      weightedsumV = weightedsumV + valid_voltage[i] * Weights[i]
       
-   weightavgV = [weightedsumV/sum(Weights)] #Divide by the sum of the weights,
-   #not amount of elements, big issue I had.
+   weightavgV = [weightedsumV / sum(Weights)] 
    return weightavgV
    print("Weighted Average for Filtered Voltages: %s"  % weightavgV)
    
-#print(Valids[0][0]) #First number is index, second is Voltage Or Current or Weight
-
 
 
 outputs = []
 for filename in os.listdir():
-    if filenameendswith.("OCV.csv"):
+    if filename.endswith("OCVData.csv"):
       df = pd.read_csv(filename) #Reading the CSV File, 1000 entries
 
       voltage= (df['voltage']) #Storing each column into a DataFrame
       current= (df['current']).multiply(10000000000) #Multiplied by 10billion or e10
       
-      outputs.append(Calculation())
-      #Graphing(filename)
+      outputs.append(calculation())
+      graphing(filename)
       
-#df = pd.DataFrame(outputs)
-#df.to_csv("OutPuts.csv")
+df = pd.DataFrame(outputs)
+df.to_csv("OutPuts.csv")
