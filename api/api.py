b564ba1dd7190ced32c138a077b72c7a54afe0ce
e-wai <e.wai.7155@gmail.com>
2020-06-13 16:56:17 -0400

    Configuring existing API funcitonality to include database.

--- api/api.py
+++ api/api.py
@@ -1,20 +1,26 @@
 from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+import sys
+import os.path
+
 import processing
+
+DATABASE_NAME = 'test.db'
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///" + os.path.join(sys.path[0], DATABASE_NAME)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# current_data = [
-#     {
-#         'velocity': 0,
-#         'recommended_velocity': 0,
-#         'elevation': 10
-#     }
-# ]
+db = SQLAlchemy(app)
+db.create_all()
+
+ma = Marshmallow(app)
 
 @app.route('/', methods=['GET'])
 def home():
-    return '<p>success</p>'
+    return 'success'
 
 @app.route('/mobile', methods=['GET'])
 def mobileData():
--- api/processing.py
+++ api/processing.py
@@ -1,25 +1,44 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
 import requests
 import sys
 import os
 sys.path.append('../')
 from optimization.car_model import Car
+from api.log_entry import Log, LogSchema
 
 DATA_API_ENDPOINT = 'http://api.open-notify.org/iss-now.json'
 
+# expected_data_format = [{
+#         'velocity': 0,
+#         'recommended_velocity': 0,
+#         'elevation': 10
+# }]
+
+car = Car()
+
+db = SQLAlchemy()
+ma = Marshmallow()
+
+log_schema = LogSchema(strict=True)
+logs_schema = LogSchema(strict=True, many=True)
+
 def getMobileData():
+
     current_data = requests.get(DATA_API_ENDPOINT)
     
     if (current_data.status_code != 200):
         return [{}]
-    
     current_data = current_data.json()
 
     response = {}
-    
     response["velocity"] = current_data["iss_position"]["longitude"]
     response["recommended_velocity"] = current_data["iss_position"]["latitude"]
-
-    car = Car()
     response["elevation"] = car.force_req(current_data["timestamp"])
+
+    new_entry = Log(response["velocity"], response["recommended_velocity"], response["elevation"])
+    print(new_entry)
+    db.session.add(new_entry)
+    db.session.commit()
 
     return([response])
