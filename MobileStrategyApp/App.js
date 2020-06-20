01576f6c2874398f21c68051f1c63558b10f2826
e-wai <e.wai.7155@gmail.com>
2020-06-20 16:11:42 -0400

    Reconfigure so that it fetches from endpoint every 3 seconds. Works, but endpoint not set up at the moment so produces type error every time.

--- MobileStrategyApp/App.js
+++ MobileStrategyApp/App.js
@@ -19,16 +19,55 @@ import {
 } from 'react-native';
 import {Header} from 'react-native-elements';
 
-const data = [
-  {id: "velocity", displayName: "Velocity (km/h)", value: 10},
-  {id: "recommendedVelocity", displayName: "Recommended Velocity (km/h)", value: 11},
-  {id: "elevation", displayName: "Elevation (km)", value: 12}
-];
+// const data = [
+//   {id: "velocity", displayName: "Velocity (km/h)", value: 10},
+//   {id: "recommendedVelocity", displayName: "Recommended Velocity (km/h)", value: 11},
+//   {id: "elevation", displayName: "Elevation (km)", value: 12}
+// ];
+
+//api_form = {
+//   "elevation": 2.33051380781163e+17, 
+//   "entry_time": "2020-06-20T17:43:22.981400", 
+//   "recommended_velocity": -39.8884, 
+//   "velocity": 17.096
+// }
 const numColumns = 2;
 const sizeOfBoxes = Dimensions.get('window').width / numColumns * 0.75;
 const heightOfPage = Dimensions.get('window').height;
+const STRATEGY_ENDPOINT = "10.0.2.2";
 
 export default class App extends React.Component {
+  state = {
+    carData: []
+  }
+
+  async componentDidMount() {
+    setInterval(async () => {
+      fetch(STRATEGY_ENDPOINT)
+      .then(res => res.json())
+      .then(data => {
+        //transform array somehow into form we want
+        usable_form = [];
+        for (ele in data) {
+            current = {};
+            current["id"] = ele;
+            current["value"] = data[ele];
+
+            usable_form.append(current);
+        }
+
+        console.log(usable_form);
+
+        this.setState((state) => {
+          return {carData: usable_form};
+        });
+      })
+      .catch((error) => {
+        console.log(`Something went wrong... ${error}`);
+      });
+    }, 3000);
+  }
+
   render() {
     return (
       <>
@@ -41,10 +80,10 @@ export default class App extends React.Component {
             <Text>Insert some graphic here</Text>
           </View>
           <FlatList
-            data = {data}
+            data = {this.state.carData}
             renderItem={({item}) => (
               <View style={styles.itemStyles}>
-                <Text style={styles.item}>{item.value}{"\n"}{item.displayName}</Text>
+                <Text style={styles.item}>{item.value}{"\n"}{item.id}</Text>
               </View>
             )}
             keyExtractor={item => item.id}
