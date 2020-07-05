d2928d70c992c56ec0f26d3edd7f092ce5a15b83
e-wai <e.wai.7155@gmail.com>
2020-07-05 19:46:06 -0400

    Change configuration setup. Currently still under DEBUG.

--- api/__init__.py
+++ api/__init__.py
@@ -5,16 +5,27 @@ import sys
 import os.path
 sys.path.append('../')
 
-DATABASE_NAME = 'db.sqlite'
+class ProdConfig:
+    FLASK_ENV = 'production'
+    DEBUG = False
+    TESTING = False
+    DATABASE_NAME = 'data.sqlite'
+
+class DevConfig:
+    FLASK_ENV = 'development'
+    DEBUG = True
+    TESTING = True
+    DATABASE_NAME = 'db.sqlite'
+
+
 DATA_API_ENDPOINT = 'http://api.open-notify.org/iss-now.json'
 
-print("hello")
-
 app = Flask(__name__)
-app.config['DEBUG'] = True
 basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///" + os.path.join(basedir, DATABASE_NAME)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///" + os.path.join(basedir, DevConfig.DATABASE_NAME)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+app.config.from_object(DevConfig)
 
 db = SQLAlchemy()
 db.init_app(app)
