223ed0b8f021d7e8e5f87bb718f2d30319f37372
Emma Wai <40974372+e-wai@users.noreply.github.com>
2020-10-18 15:23:14 -0400

    File path dashboard fix (#84)
    * File paths referenced differently on windows so does not work. Use
    absolute file paths and os.path.join to fix.
    
    * Add README.md with instructions on running

--- displays/dashboard/route.py
+++ displays/dashboard/route.py
@@ -5,12 +5,16 @@ import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output, State
 from dash.exceptions import PreventUpdate
 import plotly.graph_objs as go
-
 import pandas as pd
+
+import sys
+import os.path
+sys.path.append(os.path.dirname(__file__))
+ELEVATIONS_FILE = os.path.join(sys.path[0], 'assets', 'wsc_elevation.csv')
 
 from application import dash_app, app
 
-TEST_DATA = pd.read_csv("displays\\dashboard\\assets\\wsc_elevation.csv", usecols=['Distance', 'Elevation (m)'], index_col='Distance', squeeze=True)
+TEST_DATA = pd.read_csv(ELEVATIONS_FILE, usecols=['Distance', 'Elevation (m)'], index_col='Distance', squeeze=True)
 
 graph1 = dbc.Col([
     dbc.Card([
