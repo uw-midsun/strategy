ebf5b4026bfe5bb937f86eadb2a912798e6debae
Janet Chen <68651393+j985chen@users.noreply.github.com>
2020-12-01 22:49:19 -0500

    [STRAT-101] Added force due to acceleration to calculations of force, energy, and torque in alt_gain.py (#90)
    * Add onboarding message.
    
    * Testing git (#67)
    
    Aryaman's first PR to the repo! Congrats!
    
    * trying to fix merge conflicts with README.md
    
    * considering force of acceleration in energy, force, and torque calcs in alt_gain.py
    
    * forgot to uncomment some lines in my last commit, sorry!
    
    * fixing issues with last pr
    
    * attempt #3.
    
    Co-authored-by: e-wai <e.wai.7155@gmail.com>
    Co-authored-by: Aryaman Singh <53343807+Aryaman73@users.noreply.github.com>

--- motortorquecalculation/alt_gain.py
+++ motortorquecalculation/alt_gain.py
@@ -54,26 +54,27 @@ class Car:
     battery_size = 3.888 * 10 ** 7 # Car energy capacity in Joules
     def __init__(self, mass, CdA, Crr):
         self.mass = mass # in kg
-        self.CdA = CdA
-        self.Crr = Crr
+        self.CdA = CdA # drag area
+        self.Crr = Crr # rolling resistance
 
-    def force(self, angle, velocity):
+    def force(self, angle, velocity, timestep=30):
         Fg = self.mass * self.gravity * sin(angle_to_rad(angle))
         Ff = self.mass * self.gravity * self.Crr * cos(angle_to_rad(angle))
         Fdrag = 0.5 * velocity ** 2 * self.CdA * self.rho
-        Ft = Fg + Ff + Fdrag
+        Faccel = self.mass * (velocity / timestep) # force required to accelerate
+        Ft = Fg + Ff + Fdrag + Faccel
         return Ft
 
-    def energy_use(self, distance, angle, velocity):
-        Ft = self.force(angle, velocity)
+    def energy_use(self, distance, angle, velocity, timestep=30):
+        Ft = self.force(angle, velocity, timestep)
         E = Ft * distance
         return E
 
-    def torque_req(self, angle, velocity):
+    def torque_req(self, angle, velocity, timestep=30):
         """
         returns the torque required per wheel
         """
-        torque_per_wheel = self.force(angle, velocity) * self.wheel_radius / 2
+        torque_per_wheel = self.force(angle, velocity, timestep) * self.wheel_radius / 2
         return torque_per_wheel
 
     def speed_req(self, angle, torque):
@@ -97,89 +98,90 @@ class Car:
             required_torque = self.torque_req(angle, min_speed)
             return min_speed, required_torque
 
-parser = argparse.ArgumentParser(description='Choose a map')
-parser.add_argument('--map', help='Map to pick to race on', default='ASC')
-parser.add_argument('--solar', help='Energy we receive from the solar panel', default=0, type=float)
-parser.add_argument('--weight', help='Weight of the car in kg', default=720, type=float)
-parser.add_argument('--dir', help='Location where your strategy directory is located', type=str)
-args = parser.parse_args()
-car = Car(args.weight, 0.15, 0.0015)
-min_speed = 7 # minimum allowable speed
-if args.map == 'WSC':
-    try:
-        csv_file = pd.read_csv(args.dir + '/routemodel/routes/wsc_elevation.csv')
-    except:
-        raise('You must pass in your strategy directory location')
-    lon = csv_file.Longitude.to_list()
-    lat = csv_file.Latitude.to_list()
-    alt = csv_file['Elevation (m)'].to_list()
-    speed_req = 22.2 # 80kph in m/s
-elif args.map == 'ASC':
-    try:
-        csv_file = pd.read_csv(args.dir + '/routemodel/routes/ASC2018.csv')
-    except:
-        raise('You must pass in your strategy directory location')
-    lon = csv_file.lon.to_list()
-    lat = csv_file.lat.to_list()
-    alt = csv_file.alt.to_list()
-    speed_req = 16.6 # 60 kph in m/s
-dist = [0]
-dalt = [0]
-climb = [0]
-angles = [0]
-for i in range(len(alt) - 1):
-    dist.append(haversine((lat[i], lon[i]), (lat[i+1], lon[i+1]), unit = Unit.METERS) + dist[i])
-    dalt = alt[i+1] - alt[i]
-    angle = dalt / (dist[i+1] - dist[i])
-    angles.append(angle)
-    climb.append(57.2958 * arctan(angle))
-avg = mean(x for x in climb if x > -57.2958 * 0.0085035)
-#print(car.torque_req(avg, speed_req))
-torques = []
-breakpoint = []
-for angle in climb:
-    torques.append(car.torque_req(angle, speed_req))
-torques = linspace(5, 30, 20)
-baseline = -1 * ceil(args.solar)
-wattages = linspace(baseline, baseline + 500, 50)
-total_energies = []
-for i in range(50):
-    
-    set_torque = 14 # Nm of torque we want from the motor
-    torque_obj = TorqueCurve(set_torque)
-    wattage = wattages[i]
-    torque_obj.generate_curve()
-    efficiencies = torque_obj.curve 
-    torquess = []
-    eff = []
-    for j in range(len(efficiencies)):
-        torquess.append(efficiencies[j][0])
-        eff.append(efficiencies[j][1])
-
-    data = [(0,0)]
-    for point in range(len(dist)):
-        data.append(car.speed_torque_calculator(climb[point], speed_req, min_speed, set_torque))
-    total_energy = 0
-    full_length = True
-    for point in range(2, len(data)):
-        pre_eff_energy = car.energy_use(dist[point - 1] - dist[point - 2], climb[point - 1], data[point][0])
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Choose a map')
+    parser.add_argument('--map', help='Map to pick to race on', default='ASC')
+    parser.add_argument('--solar', help='Energy we receive from the solar panel', default=0, type=float)
+    parser.add_argument('--weight', help='Weight of the car in kg', default=720, type=float)
+    parser.add_argument('--dir', help='Location where your strategy directory is located', type=str)
+    args = parser.parse_args()
+    car = Car(args.weight, 0.15, 0.0015)
+    min_speed = 7 # minimum allowable speed
+    if args.map == 'WSC':
+        try:
+            csv_file = pd.read_csv(args.dir + '/routemodel/routes/wsc_elevation.csv')
+        except:
+            raise('You must pass in your strategy directory location')
+        lon = csv_file.Longitude.to_list()
+        lat = csv_file.Latitude.to_list()
+        alt = csv_file['Elevation (m)'].to_list()
+        speed_req = 22.2 # 80kph in m/s
+    elif args.map == 'ASC':
+        try:
+            csv_file = pd.read_csv(args.dir + '/routemodel/routes/ASC2018.csv')
+        except:
+            raise('You must pass in your strategy directory location')
+        lon = csv_file.lon.to_list()
+        lat = csv_file.lat.to_list()
+        alt = csv_file.alt.to_list()
+        speed_req = 16.6 # 60 kph in m/s
+    dist = [0]
+    dalt = [0]
+    climb = [0]
+    angles = [0]
+    for i in range(len(alt) - 1):
+        dist.append(haversine((lat[i], lon[i]), (lat[i+1], lon[i+1]), unit = Unit.METERS) + dist[i])
+        dalt = alt[i+1] - alt[i]
+        angle = dalt / (dist[i+1] - dist[i])
+        angles.append(angle)
+        climb.append(57.2958 * arctan(angle))
+    avg = mean(x for x in climb if x > -57.2958 * 0.0085035)
+    #print(car.torque_req(avg, speed_req))
+    torques = []
+    breakpoint = []
+    for angle in climb:
+        torques.append(car.torque_req(angle, speed_req))
+    torques = linspace(5, 30, 20)
+    baseline = -1 * ceil(args.solar)
+    wattages = linspace(baseline, baseline + 500, 50)
+    total_energies = []
+    for i in range(50):
         
-        if above(torque_obj.torque_list, data[point][1]) != -1:
-            efficiency = efficiencies[above(torque_obj.torque_list, data[point][1])][1] 
-        else:
-            efficiency = 0 
-        total_energy += pre_eff_energy + wattage * (dist[point - 1] - dist[point - 2]) / data[point][0]
-        if total_energy > Car.battery_size:
-            breakpoint.append(dist[point - 1])
-            full_length = False
-            break
-    total_energies.append(total_energy)
-    if full_length:
-        breakpoint.append(dist[-1])
-break_in_km = [x / 1000 for x in breakpoint]
-normalized_wattages = [x - baseline for x in wattages]
-plt.xlabel('Added Energy Use (W)')
-plt.ylabel('Range (km)')
-plt.title(args.map)
-plt.plot(normalized_wattages, break_in_km)
-plt.show()
+        set_torque = 14 # Nm of torque we want from the motor
+        torque_obj = TorqueCurve(set_torque)
+        wattage = wattages[i]
+        torque_obj.generate_curve()
+        efficiencies = torque_obj.curve 
+        torquess = []
+        eff = []
+        for j in range(len(efficiencies)):
+            torquess.append(efficiencies[j][0])
+            eff.append(efficiencies[j][1])
+
+        data = [(0,0)]
+        for point in range(len(dist)):
+            data.append(car.speed_torque_calculator(climb[point], speed_req, min_speed, set_torque))
+        total_energy = 0
+        full_length = True
+        for point in range(2, len(data)):
+            pre_eff_energy = car.energy_use(dist[point - 1] - dist[point - 2], climb[point - 1], data[point][0])
+            
+            if above(torque_obj.torque_list, data[point][1]) != -1:
+                efficiency = efficiencies[above(torque_obj.torque_list, data[point][1])][1] 
+            else:
+                efficiency = 0 
+            total_energy += pre_eff_energy + wattage * (dist[point - 1] - dist[point - 2]) / data[point][0]
+            if total_energy > Car.battery_size:
+                breakpoint.append(dist[point - 1])
+                full_length = False
+                break
+        total_energies.append(total_energy)
+        if full_length:
+            breakpoint.append(dist[-1])
+    break_in_km = [x / 1000 for x in breakpoint]
+    normalized_wattages = [x - baseline for x in wattages]
+    plt.xlabel('Added Energy Use (W)')
+    plt.ylabel('Range (km)')
+    plt.title(args.map)
+    plt.plot(normalized_wattages, break_in_km)
+    plt.show()
