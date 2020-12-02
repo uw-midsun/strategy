1e89145566a5451df3fa53edd4573e1e2bfdf935
krishnakalavadia <kkalavad@uwaterloo.ca>
2020-12-01 20:26:47 -0500

    completed elevations file refactor

--- routemodel/data_retrieval/config_example.py
+++
@@ -1,4 +0,0 @@
-# Instructions: Create "config.py" file in this folder
-# Add API_KEY as shown
-# get_elevations and get_navigation should be pass pytest once done! Make sure config.py never being pushed up.
-API_KEY = ''
