a3f1024187383a94f31e9c18c2392b1a48bf9052
e-wai <e.wai.7155@gmail.com>
2021-01-21 19:41:17 -0500

    Sort through firmware samples

---
+++ firmware_data.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import os
+import sys
+
+DATA = "can_messages.csv"
+
+df = pd.read_csv(os.path.join(sys.path[0], DATA))
+df.dropna(inplace=True)
+df.drop(df[df.sender != "BMS_CARRIER"].index, inplace=True)
+print(df)
