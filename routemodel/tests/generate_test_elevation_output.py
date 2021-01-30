151df1318b47ce1ecaafdaee635caa86ca8493b4
Min Kim <minkim0320@gmail.com>
2021-01-30 14:43:06 -0500

    [STRAT-138] Add script to test elevation with sample points

--- routemodel/tests/generate_test_elevation_output.py
+++ routemodel/tests/generate_test_elevation_output.py
@@ -8,24 +8,39 @@ import data_retrieval.elevations as elevs
 import csv
 
 data_file = os.path.join(os.path.dirname(__file__), '..', 'routes', 'ASC2018.csv')
+output_file  = os.path.join(os.path.dirname(__file__), 'test_elevation_output.csv')
 
 def test_elevations_points_builder_and_query(points: list):
     points_encoded = elevs.build_elevations_points(points)
     query = elevs.format_elevations_query(points_encoded)
     query_polyline = elevs.format_elevations_query(points_encoded, 'polyline', 10)
     query_ellipsoid = elevs.format_elevations_query(points_encoded, heights='ellipsoid')
+    elevations_data = [points_encoded, query, query_polyline, query_ellipsoid]
+    write_csv_file(elevations_data)
+
+def write_csv_file(elevations_data: list):
+    with open(output_file, mode='a', newline='') as write_file:
+        file_writer = csv.writer(write_file, delimiter=",")
+        file_writer.writerow(elevations_data)
+
+def reset_csv_file():
+    with open('test_elevation_output.csv', mode='w', newline='') as clear_file:
+        clear_file.truncate()
+        file_writer = csv.writer(clear_file, delimiter=",")
+        file_writer.writerow(['points_encoded', 'query', 'query_polyline', 'query_ellipsoid'])
 
 with open(data_file, "r") as file:
     reader = csv.reader(file)
     points = []
     count = 0
+    reset_csv_file()
     for row in reader:
         if (row[1] == 'lon'):
             continue
         points.append({row[1]:row[2]})
         count += 1
-        if (count==10):
+        if (count%10 == 0):
+            test_elevations_points_builder_and_query(points)
+            points.clear()
+        if count == 100:
             break
-    print(points)
-    test_elevations_points_builder_and_query(points)
-    
