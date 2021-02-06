926f7346d815fd39029387f4faf764ed4f6dda13
klinhdo <klinhdo@users.noreply.github.com>
2021-02-06 11:52:44 -0500

    Make API call to BingsMap

--- routemodel/tests/generate_test_elevation_output.py
+++ routemodel/tests/generate_test_elevation_output.py
@@ -6,8 +6,9 @@ sys.path.append(os.path.dirname(sys.path[0]))
 import pandas as pd
 import data_retrieval.elevations as elevs
 import csv
+import data_retrieval.common as commons
 
-data_file = os.path.join(os.path.dirname(__file__), '..', 'routes', 'ASC2018.csv')
+data_file = os.path.join(os.path.dirname(__file__), '..', 'routes', 'ASC2021','ASC2021_elevations_draft.csv')
 output_file  = os.path.join(os.path.dirname(__file__), 'test_elevation_output.csv')
 
 def test_elevations_points_builder_and_query(points: list):
@@ -15,8 +16,17 @@ def test_elevations_points_builder_and_query(points: list):
     query = elevs.format_elevations_query(points_encoded)
     query_polyline = elevs.format_elevations_query(points_encoded, 'polyline', 10)
     query_ellipsoid = elevs.format_elevations_query(points_encoded, heights='ellipsoid')
-    elevations_data = [points_encoded, query, query_polyline, query_ellipsoid]
-    write_csv_file(elevations_data)
+    
+    response_default = commons.get_API_data(query)
+    
+    parsed_response = elevs.parse_elevations_data(response_default, points, method='default')
+    print(parsed_response)
+    
+    response_polyline = commons.get_API_data(query_polyline)
+    response_ellipsoid = commons.get_API_data(query_ellipsoid)
+            
+    #elevations_data = [response_default, response_polyline, response_ellipsoid]
+    #write_csv_file(elevations_data)
 
 def write_csv_file(elevations_data: list):
     with open(output_file, mode='a', newline='') as write_file:
@@ -35,12 +45,8 @@ with open(data_file, "r") as file:
     count = 0
     reset_csv_file()
     for row in reader:
-        if (row[1] == 'lon'):
+        if (row[1] == 'latitude'):
             continue
         points.append({row[1]:row[2]})
         count += 1
-        if (count%10 == 0):
-            test_elevations_points_builder_and_query(points)
-            points.clear()
-        if count == 100:
-            break
+    test_elevations_points_builder_and_query(points)
