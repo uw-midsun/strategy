5a229796c7606ae712a1aa7cb1f5dbb485f8363d
knwk <keonuw@icloud.com>
2021-02-13 13:38:09 -0500

    Outputs generated_speedlimits.csv file

--- routemodel/data_retrieval/generate_test_speedlimits.py
+++ routemodel/data_retrieval/generate_test_speedlimits.py
@@ -3,22 +3,25 @@ import sys
 import os.path
 import csv
 import speedlimits as spdlts
+import common
 
 sys.path.append(os.path.dirname(__file__))
-ASC2021_file = "../routes/ASC2021/ASC2021_draft.csv"
+input_file = os.path.join(os.path.dirname(__file__), '..', 'routes', 'ASC2021','ASC2021_draft.csv')
+output_file  = os.path.join(os.path.dirname(__file__), 'generated_speedlimits.csv')
 
 
 def generate_speedlimits(points: list):
     pts = spdlts.build_speedlimits_points(points)
     query = spdlts.format_speedlimits_query(pts)
-    spdlts_data = [pts, query]
+    response = common.get_API_data(query)
+    parsed_df = spdlts.parse_speedlimits_data(response)
+    
+    return parsed_df
 
-    return spdlts_data
 
-
-def read_csv():
+def read_csv(file_path):
     points = []
-    with open(ASC2021_file) as csv_file:
+    with open(file_path) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         for row in csv_reader:
             points.append({row[0]: row[1]})
@@ -26,22 +29,10 @@ def read_csv():
     return points
 
 
-def write_to_csv(output_file, data: list):
-    with open(output_file, mode='w') as csv_file:
-        writer = csv.writer(csv_file, delimiter=",", quoting=csv.QUOTE_MINIMAL)
-        writer.writerow(data)
-
-
 def main():
-
-    points = read_csv()
-    data = generate_speedlimits(points)
-
-    # Read ASC2021 CSV File
-    # Take it as input into generate_speedlimits(points:list)
-    # Need API token to make a get request to get speedlimits
-    # Take the manipulated spdlts_data and write to a new csv file
-    #   This CSV file would have the outputs that we want to analyze
+    points = read_csv(input_file)
+    parsed_df = generate_speedlimits(points)
+    parsed_df.to_csv(output_file)
 
 
 if __name__ == "__main__":
