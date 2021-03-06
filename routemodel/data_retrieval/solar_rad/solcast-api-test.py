81cee50143e86f178175c8b7b52c9b8bc946d55e
Linna Luo <linnaluo18@gmail.com>
2021-03-06 12:26:57 -0500

    saving changes

--- routemodel/data_retrieval/solar_rad/solcast-api-test.py
+++ routemodel/data_retrieval/solar_rad/solcast-api-test.py
@@ -1,7 +1,7 @@
 import json
 import requests
 
-api_key = 'na'
+api_key = 'IVUgOl4ATKF3lmhJv2kmhl8APY4KXxu6'
 base_url = 'https://api.solcast.com.au/'
 
 headers = {'Content-Type': 'application/json',
@@ -9,18 +9,26 @@ headers = {'Content-Type': 'application/json',
 
 def get_radiation_data(latitude, longitude):
     #creating dictionary of parameters (not sure if I should be creating this here or not)
-    params = {
+    params = [
+        {
         'latitude': latitude,
         'longitude': longitude
-    }
+        },
+        {
+        'latitude': latitude,
+        'longitude': longitude
+        }
+    ]
 
     api_url = base_url + '/world_radiation/forecasts'
     response = requests.get(api_url, headers=headers, params=params)
     if response.status_code == 200:
         return json.loads(response.content.decode('utf-8'))
     else:
+        print(response.status_code)
         return None
 
 #driver code
 ret = get_radiation_data(-80.547245,43.473550) #UW coordinates
-print(ret)
+with open('solcast-response.txt', 'w') as f:
+    f.write(json.dumps(ret))
