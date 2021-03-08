659fddda49efc728b67bf6eed1029b57b4838007
Rodrigo <rodrigotiscareno1@gmail.com>
2021-03-08 16:56:34 -0500

    Fixed Test

--- solar/tests/test_cell.py
+++ solar/tests/test_cell.py
@@ -10,7 +10,4 @@ def test_efficiency_decrease_under_25():
 
 def test_efficiency_decrease_over_25():
     cell = SolarCell(0,100)
-    temp = round(cell.total_efficiency_decrease(),6)
-    assert(temp == 0.454500) 
-    #This test is incorrect because as of writing this the efficiency_deccrease funtion is broken
-    
+    assert(round(cell.total_efficiency_decrease(),6) == 0.0255) 
