6686768e3f2a02f328d34a033f63c916e5cf2824
haroutkhach <77372960+haroutkhach@users.noreply.github.com>
2021-05-06 18:34:31 -0400

    [STRAT-142] Calculate Expected Times to Reach Certain Longitude/Latitudes (#117)
    * Add a random space in a comment
    
    * Time to location function
    
    * Add time_to function to routes.py and change implementation to add speed, elapsed time, and timestamp columns
    
    * Change timestamp name, fix bug from referencing next segment speed instead of current, and add tests
    
    * Remove main call in tests file
    
    * Remove space
    
    * Fix unused import
    
    * Add error test and README mention of new method
    
    Co-authored-by: e-wai <e.wai.7155@gmail.com>

--- routemodel/tests/test_routes.py
+++ routemodel/tests/test_routes.py
@@ -4,6 +4,9 @@ sys.path.append(os.path.dirname(sys.path[0]))
 
 import pandas as pd
 import data_retrieval.routes as rts
+from datetime import datetime, timedelta
+
+import pytest
 
 def test_routes_points_builder_wps_vwps_correct():
     waypoints = [{"45": "-74"}, {"46": "-75"}]
@@ -86,4 +89,61 @@ def test_parse_routing_data_successful_response():
                   'Direction':['N'],
                   'Street':['Main Street']}
     check = pd.DataFrame(data_check, columns = list(data_check.keys()))
-    pd.util.testing.assert_frame_equal(data, check, check_dtype=False)
+    pd.testing.assert_frame_equal(data, check, check_dtype=False)
+
+def test_time_to_speeds_equivalent_length():
+    sample_data = {'Latitude':[5, 6],
+                  'Longitude':[10, 16], 
+                  'Maneuver Instruction':['Keep swimming', 'why'],
+                  'Distance to Maneuver':[2, 1.5],
+                  'Direction':['N', 'S'],
+                  'Street':['Main Street', 'yes']}
+    sample_data_df = pd.DataFrame(sample_data, columns = list(sample_data.keys()))
+
+    speeds = [2, 3]
+    sample_data['Speed'] = speeds
+    sample_data['Elapsed Time'] = [1, 0.5]
+    start_time = datetime(2020, 12, 8, 1, 2, 3)
+    sample_data['Start Timestamp'] = [start_time, start_time + timedelta(hours=1)]
+    result_df = pd.DataFrame(sample_data, columns=list(sample_data.keys()))
+
+    pd.testing.assert_frame_equal(
+        rts.time_to(speeds, sample_data_df, start_time), 
+        result_df
+    )
+
+def test_time_to_single_speed():
+    sample_data = {'Latitude':[5, 6],
+                  'Longitude':[10, 16], 
+                  'Maneuver Instruction':['Keep swimming', 'why'],
+                  'Distance to Maneuver':[2, 1.5],
+                  'Direction':['N', 'S'],
+                  'Street':['Main Street', 'yes']}
+    sample_data_df = pd.DataFrame(sample_data, columns = list(sample_data.keys()))
+
+    speeds = [2]
+    sample_data['Speed'] = [2] * 2
+    sample_data['Elapsed Time'] = [1, 0.75]
+    start_time = datetime(2020, 12, 8, 1, 2, 3)
+    sample_data['Start Timestamp'] = [start_time, start_time + timedelta(hours=1)]
+    result_df = pd.DataFrame(sample_data, columns=list(sample_data.keys()))
+
+    pd.testing.assert_frame_equal(
+        rts.time_to(speeds, sample_data_df, start_time), 
+        result_df
+    )
+
+def test_time_to_unexpected_length_speeds():
+    sample_data = {'Latitude':[5, 6],
+                  'Longitude':[10, 16], 
+                  'Maneuver Instruction':['Keep swimming', 'why'],
+                  'Distance to Maneuver':[2, 1.5],
+                  'Direction':['N', 'S'],
+                  'Street':['Main Street', 'yes']}
+    sample_data_df = pd.DataFrame(sample_data, columns = list(sample_data.keys()))
+
+    speeds = [2, 1, 1]
+    start_time = datetime(2020, 12, 8, 1, 2, 3)
+
+    with pytest.raises(ValueError):
+        rts.time_to(speeds, sample_data_df, start_time)
