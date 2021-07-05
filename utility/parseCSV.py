df9eeeb67789904e135d17daf388267e0731b6d0
Rodrigo <rodrigotiscareno1@gmail.com>
2021-07-05 16:24:09 -0400

    Updated Function to Accept "Priority Field"

--- utility/parseCSV.py
+++ utility/parseCSV.py
@@ -1,3 +1,7 @@
+# Requirements: 
+# 1. Entering data spreadsheets to the function in order as referenced.
+# 2. Updating priority field on both spreadsheets, ensuring value is equal on both. 
+
 import pandas as pd
 
 # parseCSV will accept the BOM.csv and MS_Quote Excel File and return two seperate Excel/CSV files with winning parts. 
@@ -8,7 +12,7 @@ def parseCSV(Bom, MS_Quote):
     
     #  Create DF of relevant information.
     Bom_DF = Bom_DF[['Manufacturer Part Number', 'Description', 'Unit Price','Mfg Std Lead Time']]
-    MS_Quote_DF = MS_Quote_DF[['Mfr. #', 'Description', 'Order Unit Price','Lead Time in Weeks']]
+    MS_Quote_DF = MS_Quote_DF[['Mfr. #', 'Description', 'Order Unit Price','Lead Time in Weeks', 'Priority']] 
     Bom_DF = Bom_DF.rename(columns = {'Manufacturer Part Number':'Mfr. #'}) 
 
     # Fill Nan values with zeroes for calculation purposes.
@@ -34,24 +38,38 @@ def parseCSV(Bom, MS_Quote):
     Bom_Winners = [] # list of indices where Bom parts win
     MS_Quote_Winners = [] # list of indices where MS_Quote parts win
 
+    # Creating lists to iterate through each item and compare values for each index
     Bom_prices = result['Unit Price'].fillna(0).tolist()
     MS_prices = result['Order Unit Price'].fillna(0).tolist()
     Bom_lead = result['Mfg Std Lead Time'].tolist()
     MS_lead = result['Lead Time in Weeks'].tolist()
+    priority = result['Priority'].tolist()
     
     for i in range(len(result)):
-        if (Bom_lead[i]>15 and MS_lead[i]>15) | (Bom_lead[i]<=15 and MS_lead[i]<=15):    
-            if Bom_prices[i]>MS_prices[i]:
+        if priority[i] == 'y': # if part is a priority, lead time comparison is forefront
+            if Bom_lead[i] > MS_lead[i]:
                 MS_Quote_Winners.append(i)
-            else:
+            elif MS_lead[i] > Bom_lead[i]:
                 Bom_Winners.append(i)
-        elif Bom_lead[i]>15 & MS_lead[i]<=15:
-            MS_Quote_Winners.append(i)
-        elif Bom_lead[i]<=15 & MS_lead[i]>15:
-            Bom_Winners.append(i)
+            else: # if both lead times are equal, compare price
+                if Bom_prices[i]>MS_prices[i]:
+                    MS_Quote_Winners.append(i)
+                else:
+                    Bom_Winners.append(i)
         else:
-            Bom_Winners.append(i)
-            MS_Quote_Winners.append(i)
+            if (Bom_lead[i]>15 and MS_lead[i]>15) | (Bom_lead[i]<=15 and MS_lead[i]<=15) | (Bom_lead[i] == MS_lead[i]): 
+            # if both lead times >, <, or = 15 weeks or are equal with each other, skip to price comparison
+                if Bom_prices[i]>MS_prices[i]:
+                    MS_Quote_Winners.append(i)
+                elif Bom_prices[i]<MS_prices[i]:
+                    Bom_Winners.append(i)
+                else: # if equal prices
+                    Bom_Winners.append(i)
+                    MS_Quote_Winners.append(i)
+            elif Bom_lead[i]>15 & MS_lead[i]<=15:  
+                MS_Quote_Winners.append(i)
+            elif Bom_lead[i]<=15 & MS_lead[i]>15:
+                Bom_Winners.append(i)
     
     Bom_Win = result.loc[Bom_Winners]
     MS_Win = result.loc[MS_Quote_Winners]
@@ -60,7 +78,6 @@ def parseCSV(Bom, MS_Quote):
 
     Bom_Win.to_excel('Bom_Parts.xlsx')
     MS_Win.to_excel('MS_Quote_Parts.xlsx')
-    
-# Testing
-parseCSV('Bom.csv', 'MS Quote Request.xls')
 
+if __name__ == "__main__":
+    parseCSV('Bom.csv', 'MS Quote Request.xls')
